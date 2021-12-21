43cba53dcbbf25eb969ff53b934609d2a877d9e5
Gosia Ras <ras.malgorzata@gmail.com>
2021-12-21 09:41:28 +0100

    styling cleaned

--- bmi-calculator-react/src/Components/Paragraph/styled.js
+++ bmi-calculator-react/src/Components/Paragraph/styled.js
@@ -2,8 +2,6 @@ import styled from "styled-components";
 
 export const StyledParagraph = styled.p`
   color: #4b7bec;
-  font-weight: 300;
-  padding: 5px;
   max-width: 50%;
   border-radius: 20px;
   text-align: center;
